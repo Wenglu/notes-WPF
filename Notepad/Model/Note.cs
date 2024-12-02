f5e27f1894ba30e5d8890cb1c19c8fa2372cd9d0
Wenglu <112478429+Wenglu@users.noreply.github.com>
2024-12-02 17:44:30 +0100

    Add files via upload

---
+++ Notepad/Model/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Notatki.Model
+{
+    public class Note
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Category { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
